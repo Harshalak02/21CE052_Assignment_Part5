42eabdf6585e9333a54150add558a35882e17986
Harshalak02 <109841967+Harshalak02@users.noreply.github.com>
2022-10-06 22:38:21 +0530

    Add files via upload

---
+++ Practical1.java
@@ -0,0 +1,35 @@
+
+// Prepared by HARSHAL KARANGALE 21CE052
+// WAP to show how to create a file with different mode and methods of File class to find
+// path, directory etc.
+import java.io.*;
+
+public class Practical1 {
+    public static void main(String[] args) {
+
+        String path = "";
+        boolean bool = false;
+        try {
+            // creating new files
+            File file1 = new File("TestFile1.txt");
+            file1.createNewFile();
+            System.out.println(file1);
+            // creating new canonical from file object
+            File file2 = file1.getCanonicalFile();
+            // returns true if the file exists
+            System.out.println(file2);
+            bool = file2.exists();
+            // returns absolute pathname
+            path = file2.getAbsolutePath();
+            System.out.println(bool);
+            // if file exists
+            if (bool) {
+                // prints
+                System.out.print(path + bool);
+            }
+        } catch (Exception e) {
+            // if any error occurs
+            e.printStackTrace();
+        }
+    }
+}
